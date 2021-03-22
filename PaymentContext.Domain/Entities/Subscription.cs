8c4fa3128298d94b077c5649445b45dda7f7f19a
Eduardo Lima <edulima2412@gmail.com>
2021-03-22 14:34:24 -0300

    add constructores in entities

--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -1,10 +1,41 @@
+using System;
+using System.Collections.Generic;
+
 namespace PaymentContext.Domain.Entities
 {
     public class Subscription
     {
-        public Datetime CreateDate { get; set; }
-        public Datetime LastUpdateDate { get; set; }
-        public Datetime? ExpireDate { get; set; }
-        public bool Active { get; set; }
+        private IList<Payment> _payments;
+        public Subscription(DateTime? expireDate)
+        {
+            CreateDate = DateTime.Now;
+            LastUpdateDate = DateTime.Now;
+            ExpireDate = expireDate;
+            Active = true;
+            _payments = new List<Payment>();
+        }
+
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
+        public bool Active { get; private set; }
+        public IReadOnlyCollection<Payment> Payments { get; }
+
+        public void AddPayment(Payment payment)
+        {
+            _payments.Add(payment);
+        }
+
+        public void Activate()
+        {
+            Active = true;
+            LastUpdateDate = DateTime.Now;
+        }
+
+        public void Inactivate()
+        {
+            Active = false;
+            LastUpdateDate = DateTime.Now;
+        }
     }
 }
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Jose", "Silva", "12345", "jose@email.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
