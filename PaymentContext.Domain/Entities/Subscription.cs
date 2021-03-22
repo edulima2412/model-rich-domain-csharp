2dda3a79b26825636c0a8098c701aea7329d4738
Eduardo Lima <edulima2412@gmail.com>
2021-03-22 07:50:07 -0300

    created entities

---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,10 @@
+namespace PaymentContext.Domain.Entities
+{
+    public class Subscription
+    {
+        public Datetime CreateDate { get; set; }
+        public Datetime LastUpdateDate { get; set; }
+        public Datetime? ExpireDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
